2b00aa676961a2a45a909e703bf3e2de43325bb7
Jason R. Coombs <jaraco@jaraco.com>
2021-12-14 22:16:07 -0500

    Refactor into functions

--- run.py
+++ run.py
@@ -1,32 +1,48 @@
-__requires__ = ['keyring', 'requests_toolbelt']
+__requires__ = ['keyring', 'requests_toolbelt', 'autocommand']
 
 
-from requests_toolbelt import sessions
 import keyring
+import autocommand
+from requests_toolbelt import sessions
 
 
-session = sessions.BaseUrlSession('https://api.wildapricot.org/v2.1/')
-auth_session = sessions.BaseUrlSession('https://oauth.wildapricot.org/')
+def build_query(**kwargs):
+    return {
+        f'${key.rstrip("_")}': value
+        for key, value in kwargs.items()
+    }
 
-client_id = 'jaraco'
-client_secret = keyring.get_password('https://api.wildapricot.org/', client_id)
-client_auth = client_id, client_secret
 
-username = 'jaraco@jaraco.com'
-user_password = keyring.get_password('https://pghfreethought.org/', username)
-auth_request = dict(
-    grant_type='password', username=username, password=user_password,
-    scope='auto',
-)
-resp = auth_session.post('auth/token', auth=client_auth, data=auth_request)
-resp.raise_for_status()
+def setup_session():
+    session = sessions.BaseUrlSession('https://api.wildapricot.org/v2.1/')
+    auth_session = sessions.BaseUrlSession('https://oauth.wildapricot.org/')
 
-session.headers['Authorization'] = f'Bearer {resp.json()["access_token"]}'
-account, = session.get('accounts').json()
+    client_id = 'jaraco'
+    client_secret = keyring.get_password('https://api.wildapricot.org/', client_id)
+    client_auth = client_id, client_secret
 
-contacts_url = next(
-    res for res in account['Resources'] if res['Name'] == 'Contacts')['Url']
+    username = 'jaraco@jaraco.com'
+    user_password = keyring.get_password('https://pghfreethought.org/', username)
+    auth_request = dict(
+        grant_type='password', username=username, password=user_password,
+        scope='auto',
+    )
+    resp = auth_session.post('auth/token', auth=client_auth, data=auth_request)
+    resp.raise_for_status()
 
-members_query = {'$filter': 'member eq true', '$async': 'false'}
-members_resp = session.get(contacts_url, data=members_query).json()
-members = session.get(members_resp['ResultUrl']).json()['Contacts']
+    session.headers['Authorization'] = f'Bearer {resp.json()["access_token"]}'
+    return session
+
+
+@autocommand.autocommand(__name__)
+def main():
+    session = setup_session()
+
+    account, = session.get('accounts').json()
+
+    contacts_url = next(
+        res for res in account['Resources'] if res['Name'] == 'Contacts')['Url']
+
+    members_query = build_query(filter='member eq true', async_='false')
+    members_resp = session.get(contacts_url, data=members_query).json()
+    members = session.get(members_resp['ResultUrl']).json()['Contacts']
